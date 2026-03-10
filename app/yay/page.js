4688e4148ca54448cefe86d2a961d55894271b13
Even Owen Thamrin <115165108+evenowen7@users.noreply.github.com>
2026-03-10 14:06:53 +0700

    copy and update text

--- app/yay/page.js
+++ app/yay/page.js
@@ -3,7 +3,7 @@ export default function page() {
   return (
     <div className='box'>
       <img alt='yayyy' className='gif' src='/gumball-and-darwing.gif' />
-      {'Yayyy~ lets plan a date <3'}
+      {'Yayyy~ minggu depan kosongin 1 hari yaa... <3'}
     </div>
   )
 
